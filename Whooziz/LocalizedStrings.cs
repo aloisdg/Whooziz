abfdb3f28fe05659947930b9553978bd9f1000cd
aloisdg <aloisdegouvello@live.fr>
2015-03-05 15:13:06 -0800

    init files

---
+++ Whooziz/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Whooziz.Resources;
+
+namespace Whooziz
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
